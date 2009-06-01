8db2f82ac396a0f73b5ea3cfe6db0c008e9b8f73
cultsoft <cultsoft@d253b8e2-4d61-11de-9e51-a92ed582648b>
2009-06-01 20:02:30 +0000

    Create db structure for members auth data and simple blog system

--- sql/mysql.sql
+++ sql/mysql.sql
@@ -1,17 +1,84 @@
 /*
 SQLyog Community Edition- MySQL GUI v5.27
-Host - 5.1.33-community : Database - zfapp
+Host - 5.1.31-community : Database - zfapp
 *********************************************************************
-Server version : 5.1.33-community
+Server version : 5.1.31-community
 */
 
 /*!40101 SET NAMES utf8 */;
 
 /*!40101 SET SQL_MODE=''*/;
 
-
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+
+/*Table structure for table `blog_comments` */
+
+DROP TABLE IF EXISTS `blog_comments`;
+
+CREATE TABLE `blog_comments` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `post_id` bigint(20) unsigned NOT NULL DEFAULT '0',
+  `author` tinytext NOT NULL,
+  `author_email` varchar(100) NOT NULL DEFAULT '',
+  `author_url` varchar(200) NOT NULL DEFAULT '',
+  `author_ip` varchar(100) NOT NULL DEFAULT '',
+  `date` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `content` text NOT NULL,
+  `approved` varchar(20) NOT NULL DEFAULT '1',
+  `member_id` bigint(20) unsigned NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `blog_comments` */
+
+/*Table structure for table `blog_posts` */
+
+DROP TABLE IF EXISTS `blog_posts`;
+
+CREATE TABLE `blog_posts` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `blog_id` bigint(20) unsigned NOT NULL DEFAULT '0',
+  `author_id` bigint(20) unsigned NOT NULL DEFAULT '0',
+  `title` varchar(200) NOT NULL DEFAULT '',
+  `content` longtext NOT NULL,
+  `date` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `blog_posts` */
+
+/*Table structure for table `blogs` */
+
+DROP TABLE IF EXISTS `blogs`;
+
+CREATE TABLE `blogs` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `title` varchar(100) NOT NULL DEFAULT '',
+  `type` smallint(1) NOT NULL DEFAULT '1',
+  `owner_member_id` bigint(20) unsigned NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `blogs` */
+
+/*Table structure for table `members` */
+
+DROP TABLE IF EXISTS `members`;
+
+CREATE TABLE `members` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `login` varchar(60) NOT NULL DEFAULT '',
+  `password` varchar(64) NOT NULL DEFAULT '',
+  `email` varchar(100) NOT NULL DEFAULT '',
+  `role_id` smallint(1) NOT NULL DEFAULT '0',
+  `is_active` smallint(1) NOT NULL DEFAULT '1',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+/*Data for the table `members` */
+
+insert  into `members`(`id`,`login`,`password`,`email`,`role_id`,`is_active`) values (1,'admin','admin','admin@example.com',1,1);
 
 /*Table structure for table `session` */
 
