2026be9ba9932e13923be9e266e1a42ca336c980
cultsoft <cultsoft@d253b8e2-4d61-11de-9e51-a92ed582648b>
2009-06-18 15:22:02 +0000

    Moved Zend_Application option 'boostrap' from configuration.ini to index.php. In this case ini file contents only database configuration.

--- index.php
+++ index.php
@@ -9,23 +9,23 @@
  * @copyright (c) 2009 CultSoft
  * @license http://cultsoft.org.ua/platform/license.html
  */
-$pathinfo = pathinfo(__FILE__);
+$pathinfo = pathinfo ( __FILE__ );
 // Define the front controller name and docroot
-define('DOC_ROOT', $pathinfo['dirname'] . DIRECTORY_SEPARATOR);
-define('FRONT_CONTROLLER_FILE', $pathinfo['basename']);
-define('STATIC_PATH', realpath(DOC_ROOT . 'static') . DIRECTORY_SEPARATOR);
-define('VAR_PATH', realpath(dirname(__FILE__) . '/var') . DIRECTORY_SEPARATOR);
+define ( 'DOC_ROOT', $pathinfo ['dirname'] . DIRECTORY_SEPARATOR );
+define ( 'FRONT_CONTROLLER_FILE', $pathinfo ['basename'] );
+define ( 'STATIC_PATH', realpath ( DOC_ROOT . 'static' ) . DIRECTORY_SEPARATOR );
+define ( 'VAR_PATH', realpath ( dirname ( __FILE__ ) . '/var' ) . DIRECTORY_SEPARATOR );
 // Define path to application directory
-define('APPLICATION_PATH', DOC_ROOT . 'platform' . DIRECTORY_SEPARATOR);
+define ( 'APPLICATION_PATH', DOC_ROOT . 'platform' . DIRECTORY_SEPARATOR );
 // Define application environment
-define('APPLICATION_ENV', 'development');
+define ( 'APPLICATION_ENV', 'development' );
 // define('APPLICATION_ENV', 'production');
-define('CORE_PATH', DOC_ROOT . 'core' . DIRECTORY_SEPARATOR);
-@set_include_path(CORE_PATH . PATH_SEPARATOR . './');
+define ( 'CORE_PATH', DOC_ROOT . 'core' . DIRECTORY_SEPARATOR );
+@set_include_path ( CORE_PATH . PATH_SEPARATOR . './' );
 /**
  * * Zend_Application
  */
 require_once 'Zend/Application.php';
 // Create application, bootstrap, and run
-$application = new Zend_Application(APPLICATION_ENV, VAR_PATH . 'configuration.ini');
-$application->bootstrap()->run();
+$application = new Zend_Application ( APPLICATION_ENV, array ('bootstrap' => array ('path' => APPLICATION_PATH . 'bootstrap.php' ) ) );
+$application->bootstrap ()->run ();
