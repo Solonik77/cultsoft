546a613eaeae42916027ba63fff8339df099bf8b
cultsoft <cultsoft@d253b8e2-4d61-11de-9e51-a92ed582648b>
2009-06-18 05:27:09 +0000

    Added platform files named with change case to lower.

--- core/App/Controller/Plugin/Language.php
+++ core/App/Controller/Plugin/Language.php
@@ -28,7 +28,7 @@ class App_Controller_Plugin_Language extends Zend_Controller_Plugin_Abstract
         $request_lang = $request->getParam('requestLang');
         $system_lang = (array_key_exists($request_lang, $system_locales)) ? $request_lang : $default_lang_key;
         Zend_Translate::setCache(App_Cache::getInstance('File'));
-        $translate = new Zend_Translate('csv', APPLICATION_PATH . 'Languages/', $system_lang, array('scan' => Zend_Translate::LOCALE_FILENAME , 'disableNotices' => true));
+        $translate = new Zend_Translate('csv', APPLICATION_PATH . 'languages/', $system_lang, array('scan' => Zend_Translate::LOCALE_FILENAME , 'disableNotices' => true));
         App::setTranslate($translate);
         setlocale(LC_ALL, $system_locales[$system_lang] . '.' . App::Config()->locale->charset);
         Zend_Form::setDefaultTranslator($translate);
---
+++ platform/resources/Site/models/DbTable/Acl/Roles.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * ACL Roles model
+ *
+ * @package Core
+ * @author Denysenko Dmytro
+ * @copyright (c) 2009 CultSoft
+ * @license http://cultsoft.org.ua/platform/license.html
+ */
+class Site_Model_DbTable_Acl_Roles extends App_DB_Table
+{
+
+    public function __construct ()
+    {
+        parent::__construct();
+    }
+}
